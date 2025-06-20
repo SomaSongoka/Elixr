52720971fe085e668de5efd49ffe49bae69a10f6
Josh L. Minga <joshlminga@gmail.com>
2025-06-20 19:32:31 +0300

    Operators

--- codetest.ex
+++ codetest.ex
@@ -1,24 +1,2 @@
-# Focusing on Unknown Data Types
-
-# Todo: Atoms
-# Atoms are constants whose name is their value. They can be created using the color(:)
-
-:hello_afirca
-# it's value is hello_africa
-
-IO.puts(:hello_afirca)
-
-# Todo: Strings - multi line strings
-
-"""
-This is a multi line string
-"""
-
-# Todo: Strings - Strings in Elixir are inserted between double quotes
-
-"This is a single line string"
-
-"""
-This is a multi line string
-Across multiple lines
-"""
+IO.puts(rem(10, 4))
+IO.puts(div(10, 3))
---
+++ lesson02/04-operator.ex
@@ -0,0 +1,17 @@
+# Todo: Arithmetic Operators
+
+# Todo:Arithmetic
+
+# since % is reserved in Elixir, we use rem instead
+IO.puts(rem(10, 4))
+# Above code will return 2
+
+# same for div
+# Todo: DIV
+# This function is used to get the quotient on division. -
+# It helps you divide one number by another and gives you the whole number answer only
+# — it removes the decimal (the point part).
+# todo: div(10, 3)  # 👉 gives 3 | Because 10 ÷ 3 = 3.333...
+# But div only gives you the 3 — it throws away the .333 part.
+IO.puts(div(10, 3))
+# Above code will return 2
