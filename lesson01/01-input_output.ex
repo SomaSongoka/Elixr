51bf0e072e4f5115e0872bdc300de278421e633c
Josh L. Minga <joshlminga@gmail.com>
2025-06-20 19:07:44 +0300

    Introduction to Elixr

---
+++ lesson01/01-input_output.ex
@@ -0,0 +1,18 @@
+# In elixir to make comment you use #
+
+# Todo: For Output you use IO.puts()
+
+IO.puts("Hello World")
+
+# Todo: For Input you use IO.gets()
+
+IO.gets("Enter your name: ")
+
+# Todo: To rune the code on terminal you use iex 0001_IO.ex
+
+# Todo: There is no need of ; in elixir, unless you want to write multiple lines of code in one line
+
+IO.puts("Am best in the World")
+IO.puts("and you know it")
+
+# Todo: To run the code on terminal you use iex lesson01/01-input_output.ex
---
+++ lesson01/02-naming.ex
@@ -0,0 +1,28 @@
+# todo: Naming Conventions
+
+# Todo: Identifiers
+
+# Todo -: Variables, functions, modules, etc. Can be named using snake_case
+
+# Todo -: snake_case is a naming convention where words are separated by underscores and each word is lowercase.
+
+# Todo -: For example: my_name, my_function, my_module, etc.
+
+# Todo -: snake_case is a naming convention where words are separated by underscores and each word is lowercase.
+
+# Todo -: For example: my_name, my_function, my_module, etc.
+
+# TODO : VARIABLES
+first_name = "Joshua"
+
+# in case you wish to assign to unsued variable, you can use _
+_unused_variable_sentence = "This is an unused variable"
+
+# | Context      | Convention  | Example                    |
+# | ------------ | ----------- | -------------------------- |
+# | Module       | PascalCase  | `MyApp.Accounts.User`      | -- Like Class in PHP
+# | Variable     | snake_case | `user_name`, `is_valid`    |
+# | Function     | snake_case | `create_user`, `to_string` |
+# | Macro        | snake_case | `my_macro`                 | - Macros let you generate code at compile time. => like defining reusable logic or annotations via code.
+# | Struct Field | snake_case | `:email`, `:created_at`    | - Similar to class properties or associative array keys.
+# | Atom         | snake_case | `:ok`, `:user_not_found`   | - Similar to constants or enum values
